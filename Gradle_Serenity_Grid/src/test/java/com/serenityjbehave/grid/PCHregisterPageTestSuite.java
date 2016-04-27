4e5872d8f949500f09c2d16702014da5bdf33008
selvavaith <selvav@softcrylic.co.in>
2016-04-27 20:21:49 +0530

    Try to override serentiy.properties file path

--- Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite.java
+++ Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite.java
@@ -4,4 +4,8 @@ import net.serenitybdd.jbehave.SerenityStories;
 
 public class PCHregisterPageTestSuite extends SerenityStories {
 
+	public PCHregisterPageTestSuite() {
+
+	}
+
 }
--- Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite2.java
+++ Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite2.java
@@ -1,7 +1,13 @@
 package com.serenityjbehave.grid;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import net.thucydides.core.ThucydidesSystemProperty;
 
 public class PCHregisterPageTestSuite2 extends SerenityStories {
 
+	public PCHregisterPageTestSuite2() {
+		
+		 getSystemConfiguration().setIfUndefined(ThucydidesSystemProperty.PROPERTIES.from(getEnvironmentVariables(),"serenity2.properties"),"serenity2.properties");
+		//ThucydidesSystemProperty.PROPERTIES.from(getEnvironmentVariables(), "serenity2.properties");
+	}
 }
