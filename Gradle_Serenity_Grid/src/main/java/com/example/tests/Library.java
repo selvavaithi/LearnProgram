765a3f5dbd2714cdfb77759b3d2f00700688a82a
selvavaithi <selvavaithi@gmail.com>
2016-05-02 14:56:11 +0530

    just an update in the Library.java class

--- Gradle_Serenity_Grid/src/main/java/com/example/tests/Library.java
+++ Gradle_Serenity_Grid/src/main/java/com/example/tests/Library.java
@@ -1,80 +1,41 @@
 package com.example.tests;
 
 import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Library {
-  private static WebDriver driver;
-  private static String baseUrl;
-  private boolean acceptNextAlert = true;
-  private static StringBuffer verificationErrors = new StringBuffer();
+	private static WebDriver driver;
+	String baseUrl = "https://www.google.co.in/?gfe_rd=cr&ei=TAQjV-C8A-KK8QfGlaWYDw&gws_rd=ssl";
+	private static StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass
-  public static void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.co.in/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@BeforeClass
+	public static void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void testUntitled() throws Exception {
-    driver.get(baseUrl + "/?gfe_rd=cr&ei=TAQjV-C8A-KK8QfGlaWYDw&gws_rd=ssl");
-    driver.findElement(By.linkText("Gmail")).click();
-    driver.findElement(By.id("account-chooser-add-account")).click();
-    driver.findElement(By.id("Email")).clear();
-    driver.findElement(By.id("Email")).sendKeys("sdfsdsdsdf");
-    driver.findElement(By.id("next")).click();
-  }
+	@Test
+	public void testUntitled() throws Exception {
+		driver.get(baseUrl);
+		driver.findElement(By.linkText("Gmail")).click();
+		driver.findElement(By.id("account-chooser-add-account")).click();
+		driver.findElement(By.id("Email")).clear();
+		driver.findElement(By.id("Email")).sendKeys("sdfsdsdsdf");
+		driver.findElement(By.id("next")).click();
+	}
 
-  @AfterClass
-  public static void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      Assert.fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	@AfterClass
+	public static void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
 }
