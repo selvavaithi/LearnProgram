698a4b95eac7953fdff376f6440cacb5d557c2cc
selvavaithi <selvavaithi@gmail.com>
2016-05-03 20:42:46 +0530

    added for sample app

--- Appium_Grid_Serenity/src/test/java/com/sampleapp/test/SampleAppTest.java
+++ Appium_Grid_Serenity/src/test/java/com/sampleapp/test/SampleAppTest.java
@@ -4,12 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import net.thucydides.core.ThucydidesSystemProperty;
 
 public class SampleAppTest extends SerenityStories {
 
+	public SampleAppTest() {
+		getSystemConfiguration().setIfUndefined(
+				ThucydidesSystemProperty.THUCYDIDES_DRIVER_CAPABILITIES.getPropertyName(),
+				"deviceName:192.168.56.101:5555");
+	}
+
 	@Override
 	public List<String> storyPaths() {
-		return Arrays.asList("Stories/RealBucksStories/simpleapp.story");
+		return Arrays.asList("Stories/SampleAppStories/simpleapp.story");
 	}
 
 }
