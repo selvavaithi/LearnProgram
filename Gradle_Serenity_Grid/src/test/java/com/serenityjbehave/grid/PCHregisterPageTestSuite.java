e77f34e720ef907983b46b28c819efe11de6e893
selvavaithi <selvavaithi@gmail.com>
2016-05-03 14:48:47 +0530

    change in process of suite

--- Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite.java
+++ Gradle_Serenity_Grid/src/test/java/com/serenityjbehave/grid/PCHregisterPageTestSuite.java
@@ -2,10 +2,8 @@ package com.serenityjbehave.grid;
 
 import java.util.Arrays;
 import java.util.List;
-
 import net.serenitybdd.jbehave.SerenityStories;
 import net.thucydides.core.ThucydidesSystemProperty;
-
 public class PCHregisterPageTestSuite extends SerenityStories {
 	public PCHregisterPageTestSuite() {
 		String dirPath = System.getProperty("user.dir");
