e407b2b455b4f368da7ec19c91f4736bb9af19db
selvavaithi <selvavaithi@gmail.com>
2016-05-05 19:27:24 +0530

    Appium Grid and serenit concept

--- Appium_Grid_Serenity/src/test/java/com/sampleapp/test/SampleApp_TestSuite.java
+++ Appium_Grid_Serenity/src/test/java/com/sampleapp/test/SampleApp_TestSuite.java
@@ -6,12 +6,14 @@ import java.util.List;
 import net.serenitybdd.jbehave.SerenityStories;
 import net.thucydides.core.ThucydidesSystemProperty;
 
-public class SampleAppTest extends SerenityStories {
+public class SampleApp_TestSuite extends SerenityStories {
 
-	public SampleAppTest() {
+	public SampleApp_TestSuite() {
 		getSystemConfiguration().setIfUndefined(
 				ThucydidesSystemProperty.THUCYDIDES_DRIVER_CAPABILITIES.getPropertyName(),
 				"deviceName:192.168.56.101:5555");
+		
+		//4d00af03525c80a1
 
 		getSystemConfiguration().setIfUndefined(
 				ThucydidesSystemProperty.THUCYDIDES_DRIVER_CAPABILITIES.getPropertyName(),
