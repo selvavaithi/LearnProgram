5be0c7a7a8405687b42e50fc292138e521feffac
selvavaithi <selvavaithi@gmail.com>
2016-05-06 17:56:44 +0530

    added sample for protractor code

---
+++ ProtractorAngularJS/src/com/angularjs/test/spec.js
@@ -0,0 +1,23 @@
+describe('Protractor Demo App', function() {
+
+	it('should add one and two', function() {
+		browser.get('http://juliemr.github.io/protractor-demo/');
+		element(by.model('first')).sendKeys(1);
+		element(by.model('second')).sendKeys(2);
+
+		element(by.id('gobutton')).click();
+
+		expect(element(by.binding('latest')).getText()).toEqual('3'); // This
+																		// is
+																		// wrong!
+	});
+
+	describe("im just testing this", function() {
+		it("it is easy", function() {
+			element(by.binding('latest')).getText().then(function(text) {
+				console.log(text)
+			})
+		});
+	});
+
+});
