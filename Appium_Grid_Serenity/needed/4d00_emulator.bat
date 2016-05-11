92e5a4c12c6e030fdda2533aa6a7718e76ea8eeb
selvavaithi <selvavaithi@gmail.com>
2016-05-11 13:20:40 +0530

    added the %cd%

--- Appium_Grid_Serenity/needed/4d00_emulator.bat
+++ Appium_Grid_Serenity/needed/4d00_emulator.bat
@@ -1,1 +1,1 @@
-appium -a 127.0.0.1 -p 4733 --no-reset --bootstrap-port 4738 -U 4d00af03525c80a1 --nodeconfig D:\WorkSpace\Driver\4d00appium2.json
+appium -a 127.0.0.1 -p 4733 --no-reset --bootstrap-port 4738 -U 4d00af03525c80a1 --nodeconfig %cd%\4d00appium2.json
--- Appium_Grid_Serenity/needed/Appiumgridhub.bat
+++ Appium_Grid_Serenity/needed/Appiumgridhub.bat
@@ -1,1 +1,1 @@
-java -jar D:\WorkSpace\Driver\selenium-server-standalone-2.52.0.jar -role hub http://127.0.0.1:4444/grid/console
+java -jar %cd%\selenium-server-standalone-2.52.0.jar -role hub http://127.0.0.1:4444/grid/console
