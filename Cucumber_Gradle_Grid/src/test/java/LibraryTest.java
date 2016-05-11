e9a4a682881f970766657eb90965e2f9b1cbe3b0
selvavaithi <selvavaithi@gmail.com>
2016-05-11 15:17:48 +0530

    made some changes

--- Cucumber_Gradle_Grid/src/test/java/LibraryTest.java
+++ Cucumber_Gradle_Grid/src/test/java/LibraryTest.java
@@ -8,8 +8,5 @@ import static org.junit.Assert.*;
  * @author selvav, @date 5/4/16 12:27 PM
  */
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+   
 }
