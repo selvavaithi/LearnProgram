9cf92772f7ff3dd00dbba972e80dd0aa46383a08
selvavaithi <selvavaithi@gmail.com>
2016-05-12 19:56:57 +0530

    AutoIt parameters are added, also the file full page is replaced with user.dir

--- BasicProgramLearn/src/main/java/com/website/test/AutoITforUpload.java
+++ BasicProgramLearn/src/main/java/com/website/test/AutoITforUpload.java
@@ -1,7 +1,6 @@
 package com.website.test;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,9 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Duration;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Sleeper;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutoITforUpload {
@@ -34,18 +31,20 @@ public class AutoITforUpload {
 
 	@Test
 	public void AutoitUpload() {
-		String filepath = "D:/Mine/GitHub/BasicProgramLearn/AutoItScript/unnamed.png";
+		// String filepath =
+		// "D:/Mine/GitHub/BasicProgramLearn/AutoItScript/unnamed.png";
 		WebElement btn = driver.findElement(By.cssSelector("span.btn.btn-success.fileinput-button"));
 		waitForElement(btn);
 		btn.click();
-		  String[] cmd = {filepath};
 		String file_dir = System.getProperty("user.dir");
+		String cmd = file_dir + "\\AutoItScript\\unnamed.png";
+
 		System.out.println("File directory is " + file_dir);
 
 		try {
 			Thread.sleep(3000);
 			System.out.println(file_dir + "/AutoItScript/FileUploadCode.exe");
-			Runtime.getRuntime().exec(file_dir + "/AutoItScript/FileUploadCode.exe", cmd);
+			Runtime.getRuntime().exec(file_dir + "\\AutoItScript\\FileUploadCode.exe" + " " + cmd);
 		} catch (InterruptedException | IOException e) {
 			e.printStackTrace();
 		}
@@ -54,19 +53,15 @@ public class AutoITforUpload {
 
 	@AfterClass
 	public static void TearDown() {
-
 		try {
 			Thread.sleep(5600);
-
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
 		driver.quit();
 	}
 
 	private void waitForElement(WebElement vElement) {
-
 		waitForElement.until(ExpectedConditions.visibilityOf(vElement));
 	}
 }
