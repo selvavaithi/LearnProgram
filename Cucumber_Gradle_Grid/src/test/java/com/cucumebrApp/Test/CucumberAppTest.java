833fe31f41c2bdf57ad6c217783e5234707e3125
selvavaithi <selvavaithi@gmail.com>
2016-05-18 13:58:56 +0530

    trying for serenity cucumber parallel grid execution

--- Cucumber_Gradle_Grid/src/test/java/com/cucumebrApp/Test/CucumberAppTest.java
+++ Cucumber_Gradle_Grid/src/test/java/com/cucumebrApp/Test/CucumberAppTest.java
@@ -1,11 +1,16 @@
 package com.cucumebrApp.Test;
 
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/appfeature/features")
 public class CucumberAppTest {
+
+	public CucumberAppTest() {
+		super();
+	}
 
 }
