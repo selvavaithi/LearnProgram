097e351e05dbc00706bc7a215c75df6c031e802e
selvavaithi <selvavaithi@gmail.com>
2016-05-24 18:05:24 +0530

    name change in selenium basic concept program

--- SeleniumBasicConcept/src/test/java/LibraryTest.java
+++ SeleniumBasicConcept/src/test/java/LibraryTest.java
@@ -8,8 +8,5 @@ import static org.junit.Assert.*;
  * @author selvav, @date 5/24/16 3:06 PM
  */
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+    
 }
