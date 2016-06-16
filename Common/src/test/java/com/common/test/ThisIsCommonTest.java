5a7a3a132146fb25ed66e31a240c6779f58903da
selvavaithi <selvavaithi@gmail.com>
2016-06-16 20:50:58 +0530

    changed some code in the common java project

--- Common/src/test/java/com/common/test/ThisIsCommonTest.java
+++ Common/src/test/java/com/common/test/ThisIsCommonTest.java
@@ -1,7 +1,21 @@
 package com.common.test;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class ThisIsCommonTest {
-    
+	String str;
+	public String needfulfilepath=System.getProperty("user.dir") + "\\Needfulfiles\\DataSet.xlsx";
+	private String filepath;
+
+	public String getStr() {
+		return str;
+	}
+
+	public ThisIsCommonTest() {
+		super();
+		filepath = System.getProperty("user.dir") + "\\Needful\\Book1.xlsx";
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+		System.out.println(filepath);
+	}
 }
