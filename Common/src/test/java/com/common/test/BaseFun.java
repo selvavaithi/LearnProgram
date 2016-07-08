14a631e14be0628181f3d4ba5bdf873a43100750
selvavaithi <selvavaithi@gmail.com>
2016-07-08 12:55:01 +0530

    added code for JOptionPane showConfirmDialog with only one button

--- Common/src/test/java/com/common/test/BaseFun.java
+++ Common/src/test/java/com/common/test/BaseFun.java
@@ -1,5 +1,7 @@
 package com.common.test;
 
+import javax.swing.JOptionPane;
+
 class Base {
 	int x = 20;
 
@@ -14,11 +16,20 @@ public class BaseFun extends Base {
 	public static void main(String arg[]) {
 		Base b = new BaseFun(); // upcasting
 		BaseFun bn = new BaseFun();
-		
+
 		System.out.println(b.x); // prints 20
 		System.out.println(bn.x); // prints 50
-		//This can be accsed as static way BaseFun.x yet for understanding placing it like this.
-		
+		// This can be accsed as static way BaseFun.x yet for understanding
+		// placing it like this.
+
 		Base.MethodsOverride(x);// prints 50
+
+		JOptionPane.showConfirmDialog(null, "MESSAGE", "TITLE", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+		JOptionPane.showMessageDialog(null, "Loading Complete...!!!");
+
+		Object[] options = { "OK" };
+		int n = JOptionPane.showOptionDialog(null, "Message here ", "Title", JOptionPane.PLAIN_MESSAGE,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 	}
 }
