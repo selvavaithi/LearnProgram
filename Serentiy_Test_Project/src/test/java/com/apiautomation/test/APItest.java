14507ed43586765bdd8b9fc6e61e624ebb4df8af
selvavaithi <selvavaithi@gmail.com>
2016-07-21 15:56:05 +0530

    added some code for API testing

--- Serentiy_Test_Project/src/test/java/com/apiautomation/test/APItest.java
+++ Serentiy_Test_Project/src/test/java/com/apiautomation/test/APItest.java
@@ -2,12 +2,13 @@ package com.apiautomation.test;
 
 import java.util.Arrays;
 import java.util.List;
-
 import net.serenitybdd.jbehave.SerenityStories;
 
 public class APItest extends SerenityStories {
+
 	@Override
 	public List<String> storyPaths() {
 		return Arrays.asList("Stories/APItest.story");
 	}
+
 }
