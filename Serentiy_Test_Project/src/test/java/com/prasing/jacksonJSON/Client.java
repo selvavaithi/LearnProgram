3bca6597c95ef00ce04a377e474268a22a8017fb
selvavaithi <selvavaithi@gmail.com>
2016-07-21 15:58:34 +0530

    code added for client in jacksonjson

--- Serentiy_Test_Project/src/test/java/com/prasing/jacksonJSON/Client.java
+++ Serentiy_Test_Project/src/test/java/com/prasing/jacksonJSON/Client.java
@@ -1,34 +1,27 @@
 package com.prasing.jacksonJSON;
 
-public class Client
-{
-    private String UserAgentId;
+public class Client {
+	private String UserAgentId;
+	private String DeviceType;
 
-    private String DeviceType;
+	public String getUserAgentId() {
+		return UserAgentId;
+	}
 
-    public String getUserAgentId ()
-    {
-        return UserAgentId;
-    }
+	public void setUserAgentId(String UserAgentId) {
+		this.UserAgentId = UserAgentId;
+	}
 
-    public void setUserAgentId (String UserAgentId)
-    {
-        this.UserAgentId = UserAgentId;
-    }
+	public String getDeviceType() {
+		return DeviceType;
+	}
 
-    public String getDeviceType ()
-    {
-        return DeviceType;
-    }
+	public void setDeviceType(String DeviceType) {
+		this.DeviceType = DeviceType;
+	}
 
-    public void setDeviceType (String DeviceType)
-    {
-        this.DeviceType = DeviceType;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "ClassPojo [UserAgentId = "+UserAgentId+", DeviceType = "+DeviceType+"]";
-    }
+	@Override
+	public String toString() {
+		return "ClassPojo [UserAgentId = " + UserAgentId + ", DeviceType = " + DeviceType + "]";
+	}
 }
