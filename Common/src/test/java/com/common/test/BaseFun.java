22ddd6431edf28323da4b8a999801e51ecd935a5
selvavaithi <selvavaithi@gmail.com>
2016-08-10 13:15:35 +0530

    added the random name generator

--- Common/src/test/java/com/common/test/BaseFun.java
+++ Common/src/test/java/com/common/test/BaseFun.java
@@ -2,6 +2,7 @@ package com.common.test;
 
 import javax.swing.JOptionPane;
 
+
 class Base {
 	int x = 20;
 
@@ -16,20 +17,34 @@ public class BaseFun extends Base {
 	public static void main(String arg[]) {
 		Base b = new BaseFun(); // upcasting
 		BaseFun bn = new BaseFun();
-
+		String tt = "file.ASPX";
+		System.out.println(tt.replaceAll(".ASPX", ""));
 		System.out.println(b.x); // prints 20
 		System.out.println(bn.x); // prints 50
 		// This can be accsed as static way BaseFun.x yet for understanding
 		// placing it like this.
 
 		Base.MethodsOverride(x);// prints 50
-
+		System.out.println("before");
 		JOptionPane.showConfirmDialog(null, "MESSAGE", "TITLE", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
 
-		JOptionPane.showMessageDialog(null, "Loading Complete...!!!");
-
+		JOptionPane.showMessageDialog(null, "Loading Complete...!!!"); // selected
+		
 		Object[] options = { "OK" };
 		int n = JOptionPane.showOptionDialog(null, "Message here ", "Title", JOptionPane.PLAIN_MESSAGE,
 				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		System.out.println("Random name: "+getName());
+		
 	}
+	
+	public static String getName() {
+ 		return firstCharUppercase((org.apache.commons.lang3.RandomStringUtils.randomAlphabetic(15)).replaceAll("f", "z").replaceAll("F", "Z"));
+	
+	}
+
+	private static String firstCharUppercase(String name) {
+		return Character.toString(name.charAt(0)).toUpperCase()
+				+ name.substring(1);
+	}
+
 }
