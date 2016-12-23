723df346fae38f329f8ef60a5161ff7707bef548
selvavaithi <selvavaithi@gmail.com>
2016-12-23 16:12:24 +0530

    added a common project

---
+++ CommonProject/src/main/java/com/commonmaven/test/CommonProject/AppDrive.java
@@ -0,0 +1,19 @@
+package com.commonmaven.test.CommonProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppDrive
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+    
+    public void getItforTest(){
+    	System.out.println("77777");
+    }
+}
